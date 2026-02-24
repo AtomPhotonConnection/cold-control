a9b05df473bb07e57409f06be5bb43770204887c
Matt King <48625105+M-G-King@users.noreply.github.com>
2026-02-24 19:13:06 +0000

    random script, should be removed

---
+++ quick_configure_awg_script.py
@@ -0,0 +1,70 @@
+import logging
+from math import log
+import time
+import pyvisa as visa
+import numpy as np
+import matplotlib.pyplot as plt
+
+from classes.Config import ConfigReader
+from classes.ExperimentalConfigs import Waveform, AwgConfiguration
+from instruments.Oscilloscopes.agilent_mso9254A import OscilloscopeManager
+from instruments.WX218x.awg_manager import AWGManager
+
+# ensure logging goes to the right place
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename=r"C:\pulse_shaping_data\logging\test_script2.log",
+    filemode="a",
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+_waveforms = [
+    Waveform(r"waveforms\new_Jan\tophat\tophat_1000ns.csv", 74e6, [(0, 0)]),
+    Waveform(r"waveforms\new_Jan\tophat\tophat_1000ns.csv", 55e6, [(0, 0)]),
+    Waveform(r"waveforms\marina\zeros\zero_1000.csv", 0, [(0, 0)]),
+    Waveform(r"waveforms\pulse_shaping_exp\stirap\standard_200ns_pump.csv", 61e6, [(0, 0)]),
+    Waveform(r"waveforms\pulse_shaping_exp\stirap\standard_200ns_stokes.csv", 80e6, [(0, 0)]),
+]
+
+awg_config = AwgConfiguration(
+    waveform_sequence=[[2, 3], [0, 4], [1]],
+    waveforms=_waveforms,
+    sample_rate=1.25e9,
+    burst_count=1,
+    waveform_output_channels=[1, 2, 3],
+    waveform_output_channel_lags=[0.0, 0.0, 0.0],
+    marker_width=1e-9
+)
+
+awg_config_simple = AwgConfiguration(
+    waveform_sequence=[[0]],
+    waveforms=[_waveforms[0]],
+    sample_rate=1.25e9,
+    burst_count=1,
+    waveform_output_channels=[1],
+    waveform_output_channel_lags=[0.0],
+    marker_width=1e-9
+)
+
+awg_config_justch3 = AwgConfiguration(
+    waveform_sequence=[[0]],
+    waveforms=[_waveforms[0]],
+    sample_rate=1.25e9,
+    burst_count=1,
+    waveform_output_channels=[3],
+    waveform_output_channel_lags=[0.0],
+    marker_width=1e-9
+)
+
+
+if __name__ == "__main__":
+    try:
+        # plt.plot(_waveforms[0].get(sample_rate=awg_config.sample_rate))
+        # plt.show()
+
+        awg = AWGManager()
+        awg.upload_and_arm(awg_config)
+    
+    finally:
+        awg.close()
+
